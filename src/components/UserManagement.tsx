77ff20ddee14ab616cf4d80b8a2c4c1aac28d504
KALYANISHARNAGATCS <sharnagatkalyani0812@gmail.com>
2025-10-08 16:11:22 +0530

    user management toggle modal

--- src/components/UserManagement.tsx
+++ src/components/UserManagement.tsx
@@ -83,6 +83,7 @@ export default function UserManagement({ setActiveTab }: UserManagementProps) {
         }
       );
 // dshjfhfhssjfhjdfhjdghjf
+// bzbdsbnfbndf
       if (res.data.status === "SUCCESS") {
         const allUsers: User[] = res.data.data.users || [];
         const currentUserEmail = cookies.get("userEmail");
